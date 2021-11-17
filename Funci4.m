8968f38afffe04000784cd75791f27e4254f2a77
LupitaMS <91708224+LupitaMS@users.noreply.github.com>
2021-11-16 23:19:10 -0600

    Add files via upload

---
+++ Funci4.m
@@ -0,0 +1,15 @@
+%Octave Scrpt
+% Title         :2.3 Función Inyectiva, suprayectiva y biyectiva.
+% Author        :Maria Guadalupe Martinez Santiago  
+%Date           :202123638
+%Version        :1
+%Usage          :octave>/phat/Funci4
+%Notes          :Requiere aplicacion de octave para la ejecucion y graficacion 
+
+(['FUNCION INYECTIVA']);
+%Dominio de funcion
+x=0:40;
+%Funcion
+y=x.^2;
+%Plotear funcion
+plot(x,y);
