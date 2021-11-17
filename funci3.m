e8069b4edaa21d74b43e631b5e4fbfbcd8b5e772
LupitaMS <91708224+LupitaMS@users.noreply.github.com>
2021-11-16 23:18:11 -0600

    Add files via upload

---
+++ funci3.m
@@ -0,0 +1,15 @@
+%Octave Scrpt
+% Title         :2.3 Función Inyectiva, suprayectiva y biyectiva.
+% Author        :Maria Guadalupe Martinez Santiago  
+%Date           :202123638
+%Version        :1
+%Usage          :octave>/phat/Funci3
+%Notes          :Requiere aplicacion de octave para la ejecucion y graficacion 
+
+(['FUNCION BIYECTIVA']);
+%Dominio de funcion
+x=-30:-1;
+%Funcion
+y=1\x.^3;
+%Plotear funcion
+plot(x,y);
